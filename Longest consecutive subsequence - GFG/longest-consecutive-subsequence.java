26be6265dc64d045390e9624495cdda985b57c19
Shubham Dubey <78752376+Shubham1306352@users.noreply.github.com>
2022-02-14 19:39:34 +0530

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.java
@@ -0,0 +1,102 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.flush();
+	}
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	   // add your code here
+	   Arrays.sort(arr);
+	   HashSet <Integer> hs = new HashSet <>();
+	   int max=arr[N-1];
+	   for(int i=0;i<N;i++)
+  {
+      hs.add(arr[i]);
+  }	   
+	   int count=0;
+	   int maxi=1;
+	   for(int i=0;i<max;i++)
+	   {
+	       if(hs.contains(i+1))
+	       count++;
+	       
+	       else
+	       count=0;
+	       
+	       if(count>maxi)
+	       maxi=count;
+	   }
+	   
+	   return maxi;
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	}
+}
