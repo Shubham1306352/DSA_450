974a3678582bc0b4740e3e208b9ae1318a56ae80
Shubham Dubey <78752376+Shubham1306352@users.noreply.github.com>
2022-02-19 16:59:42 +0530

    Added solution - LeetHub

--- Check for Balanced Tree - GFG/check-for-balanced-tree.java
+++ Check for Balanced Tree - GFG/check-for-balanced-tree.java
@@ -146,7 +146,7 @@ class Tree
         int rh=heightcheck(root.right);
         
         if(lh==-1)return -1;
-        if(rh==-1)return -1;
+         if(rh==-1)return -1;
         if(Math.abs(lh-rh)>1) return -1;
         
         return Math.max(lh,rh)+1;
